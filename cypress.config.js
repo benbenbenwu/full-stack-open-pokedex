c3bc767db34f86893cee62ac67cc858153d497a3
ben <ben@example.com>
2023-01-12 16:43:11 +0800

    e2e test for individual pokemon v7

--- cypress.config.js
+++ cypress.config.js
@@ -2,5 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    setupNodeEvents(_on, _congig) {
+      // implement node event listeners here
+    },
   },
 });
